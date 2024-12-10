99eb104900dc9c93223be742c2aab6cb5609b207
Mirusna <mirusnasaravanan@gmail.com>
2024-12-10 14:49:47 +0530

    Add files via upload

---
+++ days.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int days=365,years,weeks,hours;
+printf("Enter the number of days:");
+scanf("%d",&days);
+hours=days*24;
+years=(days/365);
+weeks=(days%365)/7;
+days=days-(years*365)+(weeks*7);
+printf("No of hours:%d\n",hours);
+printf("No of weeks:%d\n",weeks);
+printf("No of days:%d\n",days);
+printf("No of years:%d\n",years);
+return 0;
+}
